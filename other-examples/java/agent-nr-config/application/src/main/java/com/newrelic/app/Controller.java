18effee1b90996b4bed7e4536a2adc9967258477
Jack Berg <jberg@newrelic.com>
2024-09-27 17:37:30 -0500

    Demonstrate agent extension with span processor that updates local root span name

--- other-examples/java/agent-nr-config/application/src/main/java/com/newrelic/app/Controller.java
+++ other-examples/java/agent-nr-config/application/src/main/java/com/newrelic/app/Controller.java
@@ -4,6 +4,7 @@ import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.trace.Span;
 import java.util.Random;
+import io.opentelemetry.api.trace.Tracer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,14 +20,20 @@ public class Controller {
           .getMeter(Application.class.getName())
           .counterBuilder("my-custom-counter")
           .build();
+  private static final Tracer TRACER =
+      GlobalOpenTelemetry.get()
+          .getTracer(Application.class.getName());
+
 
   @GetMapping("/ping")
-  public String ping() {
+  public String ping() throws InterruptedException {
     // Demonstrate adding a custom attribute to the current span.
     Span.current().setAttribute("my-key", "my-value");
 
     MY_COUNTER.add(new Random().nextInt(1000));
     LOGGER.info("A sample log message!");
+
+    doWork();
 
     // Throw an exception ~25% of the time
     if (new Random().nextInt(4) == 0) {
@@ -34,5 +41,11 @@ public class Controller {
     }
 
     return "pong";
+  }
+
+  private void doWork() throws InterruptedException {
+    Span span = TRACER.spanBuilder("doWork").setAttribute("key", "value").startSpan();
+    Thread.sleep(100);
+    span.end();
   }
 }
--- other-examples/java/agent-nr-config/config-extension/src/main/java/com/newrelic/otel/extension/Customizer.java
+++ other-examples/java/agent-nr-config/config-extension/src/main/java/com/newrelic/otel/extension/Customizer.java
@@ -3,11 +3,17 @@ package com.newrelic.otel.extension;
 import static io.opentelemetry.api.common.AttributeKey.stringKey;
 
 import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
+import io.opentelemetry.instrumentation.api.instrumenter.LocalRootSpan;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
 import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.ReadWriteSpan;
+import io.opentelemetry.sdk.trace.ReadableSpan;
+import io.opentelemetry.sdk.trace.SpanProcessor;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
 import java.util.UUID;
 
@@ -28,14 +34,45 @@ public class Customizer implements AutoConfigurationCustomizerProvider {
             resource.merge(
                 Resource.builder().put(SERVICE_INSTANCE_ID, UUID.randomUUID().toString()).build()));
 
-    // Set the sampler to be the default parentbased_always_on, but drop calls to spring
-    // boot actuator endpoints
     autoConfiguration.addTracerProviderCustomizer(
-        (sdkTracerProviderBuilder, configProperties) ->
-            sdkTracerProviderBuilder.setSampler(
-                Sampler.parentBased(
-                    RuleBasedRoutingSampler.builder(SpanKind.SERVER, Sampler.alwaysOn())
-                        .drop(HTTP_ROUTE, "/actuator.*")
-                        .build())));
+        (sdkTracerProviderBuilder, configProperties) -> {
+          // Add a processor to update local root span names
+          sdkTracerProviderBuilder.addSpanProcessor(new RootSpanUpdater());
+          // Set the sampler to be the default parentbased_always_on, but drop calls to spring
+          // boot actuator endpoints
+          sdkTracerProviderBuilder.setSampler(
+              Sampler.parentBased(
+                  RuleBasedRoutingSampler.builder(SpanKind.SERVER, Sampler.alwaysOn())
+                      .drop(HTTP_ROUTE, "/actuator.*")
+                      .build()));
+          return sdkTracerProviderBuilder;
+        });
+  }
+
+  private static class RootSpanUpdater implements SpanProcessor {
+
+    @Override
+    public void onStart(Context context, ReadWriteSpan readWriteSpan) {
+
+    }
+
+    @Override
+    public boolean isStartRequired() {
+      return false;
+    }
+
+    @Override
+    public void onEnd(ReadableSpan readableSpan) {
+      // If the span matches certain conditions, update the local root span's name to this span
+      if (readableSpan.getKind() == SpanKind.INTERNAL && "value".equals(readableSpan.getAttribute(AttributeKey.stringKey("key")))) {
+        Span localRoot = LocalRootSpan.current();
+        localRoot.updateName(readableSpan.getName());
+      }
+    }
+
+    @Override
+    public boolean isEndRequired() {
+      return true;
+    }
   }
 }
